7bc6de3dd034204b209f70b0e617684a587ae2ff
Matt Cooper <matt@matthewcooper.net>
2013-01-20 23:10:08 +0000

    Tidying up apps

--- src/start_server.sh
+++ src/start_server.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-echo "############################################"
-echo "#####         Starting Server!         #####"
-echo "############################################"
-echo " "
-
 cd ../../../
+
+echo "PROGRESS:0"
+echo "Starting Server!"
 
 if vagrant hosts list >/dev/null; then
 	echo "Plugin: vagrant-hostmaster installed"
@@ -14,15 +12,22 @@ else
 	vagrant gem install vagrant-hostmaster
 fi
 
+echo "PROGRESS:10"
+
 vagrant up --no-provision
+
+echo "PROGRESS:80"
+echo "Server Started!"
+
 mkdir data
 mount -t smbfs //guest@192.168.33.10/data data
 
-echo " "
-echo "############################################"
-echo "#####          Server Booted!          #####"
-echo "############################################"
+echo "PROGRESS:90"
+echo "Shared Drive Mounted!"
 
 open provision.app
+
+echo "PROGRESS:100"
+echo "Server Booted!"
 
 sleep 1
--- stop-server.app/Contents/Resources/script
+++ stop-server.app/Contents/Resources/script
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
-echo "############################################"
-echo "#####         Stopping Server!         #####"
-echo "############################################"
-echo " "
-
 cd ../../../
+
+echo "PROGRESS:0"
+echo "Stopping Server!"
+
 umount data
 rm -rf data
+
+echo "PROGRESS:20"
+echo "Shared drive Unmounted!"
+
 vagrant halt
 
-echo " "
-echo "############################################"
-echo "#####          Server Stopped!         #####"
-echo "############################################"
+echo "PROGRESS:100"
+echo "Server Stopped!"
 
 sleep 1
